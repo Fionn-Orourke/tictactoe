735b9739c644e9dc1e5ded15ade36913cd516719
Fionn Orourke <G00429572@atu.ie>
2024-02-27 21:02:39 +0000

    grid mapping when clicked and returns values from 1-9 for each box

--- test1.cpp
+++ test1.cpp
@@ -5,10 +5,14 @@
 const int SCREEN_WIDTH = 600;
 const int SCREEN_HEIGHT = 600;
 
+int box(int , int );
+
+
+
 int main(int argc, char* args[])
 {
     Uint32 buttons;
-    int x, y;
+    int x, y, clickPos;
     //The window we'll be rendering to
     SDL_Window* window = NULL;
 
@@ -51,15 +55,23 @@ int main(int argc, char* args[])
 
             while (true) {
                 Uint32 buttons = SDL_GetMouseState(&x, &y);
-                printf_s("%d, %d", x, y);
+                //printf_s("%d, %d", x, y);
 
 
                             //Update the surface
                 SDL_UpdateWindowSurface(window);
 
                             //get window to stay up
-                SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+                SDL_Event e;
+                SDL_PollEvent(&e);
+                if (e.button.state == 1){
+                    Uint32 buttons = SDL_GetMouseState(&x, &y);
+                    printf_s(" (%d, %d)", x, y);
+                    printf_s("%i", box(x, y));
+                    }
+                
             }
+            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 
             
         }
@@ -68,9 +80,39 @@ int main(int argc, char* args[])
 
     SDL_DestroyWindow(window);
 
-    //Quit SDL subsystems
+    //Quit SDL window
     SDL_Quit();
 
     return 0;
 }
 
+
+
+
+int box(int col, int row) {
+    int result = 0;
+
+    if (col >= 52 && col <= 179)
+        result = 1;
+    else if (col >= 185 && col <= 298)
+        result = 2;
+    else if (col >= 304 && col <= 418)
+        result = 3;
+    else
+        result = 0;
+
+    if (result > 0) {
+        if (row >= 64 && row <= 178)
+            result += 0;
+        else if (row >= 182 && row <= 299)
+            result += 3;
+        else if (row >= 301 && row <= 436)
+            result += 6;
+    }
+
+    if ((col < 52 || col > 418) || (row < 64 || row > 436)) {
+        result = 0;
+    }
+
+    return result;
+}
