aeab4e2d03d4f5c2684d28f9d535e978bac80e49
Fionn Orourke <G00429572@atu.ie>
2024-03-14 21:58:29 +0000

    bot added

--- test1.cpp
+++ test1.cpp
@@ -14,43 +14,8 @@ int box(int, int);
 int i, j, count1, count2;
 char prev = 'o';
 
-
-// Function to check if there is a winner
-/*bool checkWin(char grid[3][3], char value) {
-    // Check rows and columns
-    SDL_Surface* screenSurface = NULL;
-
-    for (int p = 0; p < 3; ++p) {
-        if ((grid[p][0] == value && grid[p][1] == value && grid[p][2] == value) ||
-            (grid[0][p] == value && grid[1][p] == value && grid[2][p] == value)) {
-            return true;
-        }
-    }
-    // Check diagonals
-    if ((grid[0][0] == value && grid[1][1] == value && grid[2][2] == value)) {
-        return true;
-    }
-
-    if (grid[0][2] == value && grid[1][1] == value && grid[2][0] == value) {
-        SDL_Rect box1 = { (xmin)+(0 * xbox)  , (ymin)+(2 * ybox), 50, 50 };
-        SDL_FillRect(screenSurface, &box1, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
-
-        SDL_Rect box2 = { (xmin)+(1 * xbox)  , (ymin)+(1 * ybox), 50, 50 };
-        SDL_FillRect(screenSurface, &box2, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
-
-        SDL_Rect box3 = { (xmin)+(2 * xbox)  , (ymin)+(0 * ybox), 50, 50 };
-        SDL_FillRect(screenSurface, &box3, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
-
-        //SDL_FillRect(screenSurface, &box, SDL_MapRGB(screenSurface->format, 0, 0xFF, 0));
-
-
-        return true;
-    }
-    return false;
-}
-*/
 bool winstat = false;
-
+int bot = 2;
 int rdisp = 20, gdisp = 20;
 int main(int argc, char* args[])
 {
@@ -104,7 +69,8 @@ int main(int argc, char* args[])
             //SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255);
             //SDL_RenderDrawLine( renderer, 8, 7, 6, 255);
             //SDL_RenderPresent(renderer);
-
+            printf_s("\nenter [1] to play against a bot, enter [2] to play 2 player: ");
+            scanf_s("%i", &bot);
             while (true) {
                 Uint32 buttons = SDL_GetMouseState(&x, &y);
                 //printf_s("%d, %d", x, y);
@@ -148,24 +114,146 @@ int main(int argc, char* args[])
                     Uint32 buttons = SDL_GetMouseState(&x, &y);
 
                     //printf_s("%i", box(x, y));
-                    if (box(x, y) <= 3) {
-                        i = 0;
-                        j = box(x, y) - 1;
-                    }
-                    else if (box(x, y) <= 6) {
-                        i = 1;
-                        j = box(x, y) - 4;
-                    }
-                    else if (box(x, y) <= 9) {
-                        i = 2;
-                        j = box(x, y) - 7;
-                    }
-                    else
-                        printf_s("incorrect input");
+                    if (bot == 2 || prev == 'o') {
 
-                    //printf_s("[%i][%i]", x, y);
-                    if (grid[i][j] == 'x' || grid[i][j] == 'o')
+                        if (box(x, y) == 0)
+                            printf_s("incorrect input");
+                        else
+                        {
+
+
+                            if (box(x, y) <= 3) {
+                                i = 0;
+                                j = box(x, y) - 1;
+                            }
+                            else if (box(x, y) <= 6) {
+                                i = 1;
+                                j = box(x, y) - 4;
+                            }
+                            else if (box(x, y) <= 9) {
+                                i = 2;
+                                j = box(x, y) - 7;
+                            }
+                            else
+                                printf_s("incorrect input");
+                        }
+                    }
+                    else //if(bot == 1 && prev == 'x')
+                    {
+                        int p, b;
+                        int n, m;
+                        int rowx[3] = { 0 }; // Initialize arrays with zeros
+                        int colx[3] = { 0 };
+                        int rowo[3] = { 0 };
+                        int colo[3] = { 0 };
+
+                        for (n = 0; n < 3; n++) {
+                            for (m = 0; m < 3; m++) {
+                                if (grid[n][m] == 'x') {
+                                    colx[n] += 1;
+                                    rowx[m] += 1;
+                                }
+                                if (grid[n][m] == 'o') {
+                                    colo[n] += 1;
+                                    rowo[m] += 1;
+                                }
+                            }
+                        }
+
+                        int choice[3] = { 0 }; // Initialize choice array with zeros
+                        choice[0] = 0; // Set initial maximum count to a negative value
+
+                        for (n = 0; n < 3; n++) {
+                            if (rowx[n] > choice[0]) {
+                                choice[0] = rowx[n];
+                                choice[1] = 2;
+                                choice[2] = n;
+                            }
+
+                            if (rowo[n] > choice[0]) {
+                                choice[0] = rowo[n];
+                                choice[1] = 2;
+                                choice[2] = n;
+                            }
+
+                            if (colx[n] > choice[0]) {
+                                choice[0] = colx[n];
+                                choice[1] = 1;
+                                choice[2] = n;
+                            }
+
+                            if (colo[n] > choice[0]) {
+                                choice[0] = colo[n];
+                                choice[1] = 1;
+                                choice[2] = n;
+                            }
+                        }
+                        for (count1 = 0; count1 <= 2; count1++) {
+                            for (count2 = 0; count2 <= 2; count2++) {
+                                if (grid[count1][count2] != 'o' && grid[count1][count2] != 'x') {
+                                    p = count1;
+                                    b = count2;
+                                }
+                                
+                            }
+                        }
+                        
+
+                        if (choice[1] == 1) {
+                            for (n = 0; n < 3; n++) {
+                                if (grid[choice[2]][n] != 'x' && grid[choice[2]][n] != 'o') {
+                                    p = choice[2];
+                                    b = n;
+                                     // Exit loop once a valid cell is found
+                                }
+                                /*else {
+                                    for (count1 = 0; count1 <= 2; count1++) {
+                                        for (count2 = 0; count2 <= 2; count2++) {
+                                            if (grid[count1][count2] != 'o' && grid[count1][count2] != 'x') {
+                                                p = count1;
+                                                b = count2;
+                                            }
+                                        }
+                                    }
+                                }*/
+
+                            }
+                            
+                        }
+
+                        else if (choice[1] == 2) 
+                        {
+                            for (n = 0; n < 3; n++) {
+                                if (grid[n][choice[2]] != 'x' && grid[n][choice[2]] != 'o') {
+                                    b = choice[2];
+                                    p = n;
+                                     // Exit loop once a valid cell is found
+                                }
+                                /*else
+                                {
+                                    for (count1 = 0; count1 <= 2; count1++) {
+                                        for (count2 = 0; count2 <= 2; count2++) {
+                                            if (grid[count1][count2] != 'o' && grid[count1][count2] != 'x') {
+                                                p = count1;
+                                                b = count2;
+                                            }
+                                        }
+                                    }
+                                }*/
+                                
+                            }
+                            
+                        
+                        }
+                     i= p;
+                     j = b;
+                    }
+                     
+                    printf_s("[%i][%i]", i, j);
+                    if (grid[i][j] == 'x' || grid[i][j] == 'o') {
+
                         printf_s("This place is already taken");
+                    }
                     else {
                         if (prev == 'o') {
                             grid[i][j] = 'x';
@@ -182,86 +270,82 @@ int main(int argc, char* args[])
                             printf_s("\n");
                         }
 
-                    }
 
-                    SDL_Rect box = { (xmin)+(j * xbox)  , (ymin)+(i * ybox), 50, 50 };
-                    if (prev == 'x')
-                        SDL_FillRect(screenSurface, &box, SDL_MapRGB(screenSurface->format, 0xFF, 0, 0));
-                    else
-                        SDL_FillRect(screenSurface, &box, SDL_MapRGB(screenSurface->format, 0, 0xFF, 0));
 
-                    /*if (checkWin(grid, prev)) {
-                        printf("Player %c wins!\n", prev);
+                        SDL_Rect box = { (xmin)+(j * xbox)  , (ymin)+(i * ybox), 50, 50 };
+                        if (prev == 'x')
+                            SDL_FillRect(screenSurface, &box, SDL_MapRGB(screenSurface->format, 0xFF, 0, 0));
+                        else
+                            SDL_FillRect(screenSurface, &box, SDL_MapRGB(screenSurface->format, 0, 0xFF, 0));
 
-                    }*/
-                    char value = prev;
-                    for (int p = 0; p < 3; ++p) {
-                        if (grid[p][0] == value && grid[p][1] == value && grid[p][2] == value) {
-                            grid[0][p] = 'w';
-                            grid[1][p] = 'w';
-                            grid[2][p] = 'w';
+                        /*if (checkWin(grid, prev)) {
+                            printf("Player %c wins!\n", prev);
+
+                        }*/
+                        char value = prev;
+                        for (int p = 0; p < 3; ++p) {
+                            if (grid[p][0] == value && grid[p][1] == value && grid[p][2] == value) {
+                                grid[0][p] = 'w';
+                                grid[1][p] = 'w';
+                                grid[2][p] = 'w';
+                                winstat = true;
+
+
+                            }
+                            if (grid[0][p] == value && grid[1][p] == value && grid[2][p] == value) {
+                                grid[p][0] = 'w';
+                                grid[p][1] = 'w';
+                                grid[p][2] = 'w';
+                                winstat = true;
+
+
+                            }
+                        }
+
+                        // Check diagonals
+                        if ((grid[0][0] == value && grid[1][1] == value && grid[2][2] == value)) {
+                            grid[0][0] = 'w';
+                            grid[1][1] = 'w';
+                            grid[2][2] = 'w';
+                            winstat = true;
+
+                        }
+
+                        if (grid[0][2] == value && grid[1][1] == value && grid[2][0] == value) {
+                            grid[0][2] = 'w';
+                            grid[1][1] = 'w';
+                            grid[2][0] = 'w';
                             winstat = true;
 
 
-                        }
-                        if (grid[0][p] == value && grid[1][p] == value && grid[2][p] == value) {
-                            grid[p][0] = 'w';
-                            grid[p][1] = 'w';
-                            grid[p][2] = 'w';
-                            winstat = true;
-
 
                         }
-                    }
+                        if (winstat == false) {
+                            int cn = 0;
+                            for (count1 = 0; count1 <= 2; count1++) {
+                                for (count2 = 0; count2 <= 2; count2++) {
+                                    if (grid[count1][count2] == 'x' || grid[count1][count2] == 'o') {
+                                        cn += 1;
+                                    }
+                                }
+                            }
+                            if (cn == 9) {
+                                winstat = true;
+                            }
 
-                    // Check diagonals
-                    if ((grid[0][0] == value && grid[1][1] == value && grid[2][2] == value)) {
-                        grid[0][0] = 'w';
-                        grid[1][1] = 'w';
-                        grid[2][2] = 'w';
-                        winstat = true;
-                        /*SDL_Rect box1 = {(xmin)+(0 * xbox)  , (ymin)+(0 * ybox), 50, 50};
-                        SDL_FillRect(screenSurface, &box1, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
+                        }
+                        if (winstat == true) {
+                            for (count1 = 0; count1 <= 2; count1++) {
+                                for (count2 = 0; count2 <= 2; count2++) {
+                                    if (grid[count1][count2] != 'w') {
+                                        SDL_Rect box1 = { (xmin)+(count1 * xbox)  , (ymin)+(count2 * ybox), 50, 50 };
+                                        SDL_FillRect(screenSurface, &box1, SDL_MapRGB(screenSurface->format, 0, 0, 0));
 
-                        SDL_Rect box2 = { (xmin)+(1 * xbox)  , (ymin)+(1 * ybox), 50, 50 };
-                        SDL_FillRect(screenSurface, &box2, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
-
-                        SDL_Rect box3 = { (xmin)+(2 * xbox)  , (ymin)+(2 * ybox), 50, 50 };
-                        SDL_FillRect(screenSurface, &box3, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));*/
-                    }
-
-                    if (grid[0][2] == value && grid[1][1] == value && grid[2][0] == value) {
-                        grid[0][2] = 'w';
-                        grid[1][1] = 'w';
-                        grid[2][0] = 'w';
-                        winstat = true;
-                        /*SDL_Rect box1 = {(xmin)+(0 * xbox)  , (ymin)+(2 * ybox), 50, 50};
-                        SDL_FillRect(screenSurface, &box1, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
-
-                        SDL_Rect box2 = { (xmin)+(1 * xbox)  , (ymin)+(1 * ybox), 50, 50 };
-                        SDL_FillRect(screenSurface, &box2, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));
-
-                        SDL_Rect box3 = { (xmin)+(2 * xbox)  , (ymin)+(0 * ybox), 50, 50 };
-                        SDL_FillRect(screenSurface, &box3, SDL_MapRGB(screenSurface->format, 0, 0, 0xFF));*/
-
-                        //SDL_FillRect(screenSurface, &box, SDL_MapRGB(screenSurface->format, 0, 0xFF, 0));
-
-
-
-
-                    }
-                    if (winstat == true) {
-                        for (count1 = 0; count1 <= 2; count1++) {
-                            for (count2 = 0; count2 <= 2; count2++) {
-                                if (grid[count1][count2] != 'w') {
-                                    SDL_Rect box1 = { (xmin)+(count1 * xbox)  , (ymin)+(count2 * ybox), 50, 50 };
-                                    SDL_FillRect(screenSurface, &box1, SDL_MapRGB(screenSurface->format, 0, 0, 0));
-
+                                    }
                                 }
                             }
                         }
                     }
-
 
 
 
